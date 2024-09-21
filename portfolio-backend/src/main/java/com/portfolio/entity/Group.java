841bc9d9e6da8df8b0b4c8acd57d8bb21b7dab9b
Dominik <84574418+kerekdominik@users.noreply.github.com>
2024-09-21 16:31:14 +0200

    Escaping table names because of H2 test db

--- portfolio-backend/src/main/java/com/portfolio/entity/Group.java
+++ portfolio-backend/src/main/java/com/portfolio/entity/Group.java
@@ -3,12 +3,14 @@ package com.portfolio.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "\"group\"")
 public class Group {
     @Id
     private Long id;
--- portfolio-backend/src/main/java/com/portfolio/entity/User.java
+++ portfolio-backend/src/main/java/com/portfolio/entity/User.java
@@ -2,12 +2,14 @@ package com.portfolio.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "\"user\"")
 public class User {
     @Id
     private long id;
