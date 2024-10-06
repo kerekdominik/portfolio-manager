dc649c4ee2f253f9f57dcb8a5e3160c101ebd622
Dominik <84574418+kerekdominik@users.noreply.github.com>
2024-10-06 20:40:03 +0200

    AppModule

---
+++ portfolio-frontend/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {LoginComponent} from './components/login/login.component';
+
+
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    CommonModule,
+    LoginComponent
+  ]
+})
+export class AppModule { }
