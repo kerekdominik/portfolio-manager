2e318bf5eb2090feb442792575812ae961a3ad6c
Kerek Dominik <kerek.dominik06@gmail.com>
2024-10-12 14:36:18 +0200

    Basic Login and Registration pages, basic routing

--- portfolio-frontend/src/app/app.module.ts
+++ portfolio-frontend/src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {LoginComponent} from './components/login/login.component';
-
-
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
-
   ],
   imports: [
     CommonModule,
-    LoginComponent
+    AppRoutingModule,
+    LoginComponent,
   ]
 })
 export class AppModule { }
--- portfolio-frontend/src/app/components/login/login.component.ts
+++ portfolio-frontend/src/app/components/login/login.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor(private readonly router: Router) {}
 
+  onSubmit() {
+    console.log('Form submitted');
+  }
+
+  navigateToRegister() {
+    this.router.navigate(['/register']).then(r => console.log(r));
+  }
 }
