f530496946b843243c684a14965a0541840c86c9
Kerek Dominik <kerek.dominik06@gmail.com>
2024-10-16 23:06:07 +0200

    oauth2-redirect component

--- portfolio-frontend/src/app/app.routes.ts
+++ portfolio-frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
 import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {Oauth2RedirectComponent} from './components/oauth2-redirect/oauth2-redirect.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'oauth2/redirect', component: Oauth2RedirectComponent },
 ];
---
+++ portfolio-frontend/src/app/components/oauth2-redirect/oauth2-redirect.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-oauth2-redirect',
+  standalone: true,
+  template: `<p>Redirection is in progress</p>`
+})
+export class Oauth2RedirectComponent implements OnInit {
+
+  constructor(private readonly route: ActivatedRoute, private readonly router: Router) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      const token = params['token'];
+      if (token) {
+        localStorage.setItem('jwtToken', token);
+        this.router.navigate(['/dashboard']);
+      } else {
+        this.router.navigate(['/login']);
+      }
+    });
+  }
+}
