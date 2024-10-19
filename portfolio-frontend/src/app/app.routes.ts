c662ef5571d3997a11557ac2bbc65a755bfa289b
Dominik <84574418+kerekdominik@users.noreply.github.com>
2024-10-19 16:35:33 +0200

    Portfolio and Profile components

--- portfolio-frontend/src/app/app.routes.ts
+++ portfolio-frontend/src/app/app.routes.ts
@@ -5,10 +5,14 @@ import {DashboardComponent} from './components/dashboard/dashboard.component';
 import {Oauth2RedirectComponent} from './components/oauth2-redirect/oauth2-redirect.component';
 import {AuthGuard} from './guards/auth.guard';
 import {LoginGuard} from './guards/login.guard';
+import {PortfolioComponent} from './components/portfolio/portfolio.component';
+import {ProfileComponent} from './components/profile/profile.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent, canActivate: [LoginGuard] },
   { path: 'register', component: RegisterComponent },
   { path: 'oauth2/redirect', component: Oauth2RedirectComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'portfolio', component: PortfolioComponent, canActivate: [AuthGuard] },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
 ];
