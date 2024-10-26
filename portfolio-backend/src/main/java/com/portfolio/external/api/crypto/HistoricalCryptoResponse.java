832e2707390dfff6aff965248b8f959b0a70ab97
Dominik <84574418+kerekdominik@users.noreply.github.com>
2024-10-26 15:59:00 +0200

    External crypto api responses

---
+++ portfolio-backend/src/main/java/com/portfolio/external/api/crypto/HistoricalCryptoResponse.java
@@ -0,0 +1,45 @@
+package com.portfolio.external.api.crypto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Setter
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class HistoricalCryptoResponse {
+    private String id;
+    private String symbol;
+    private String name;
+    private MarketData marketData;
+
+    @JsonProperty("market_data")
+    public MarketData getMarketData() { return marketData; }
+
+    @Setter
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class MarketData {
+
+        @JsonProperty("current_price")
+        private Map<String, Double> currentPrice;
+
+        public Double getPriceInUSD() {
+            if (currentPrice != null && currentPrice.containsKey("usd")) {
+                return currentPrice.get("usd");
+            }
+            return null;
+        }
+
+        public Double getPriceInCurrency(String currency) {
+            currency = currency.toLowerCase();
+            if (currentPrice != null && currentPrice.containsKey(currency)) {
+                return currentPrice.get(currency);
+            }
+            return null;
+        }
+    }
+}
