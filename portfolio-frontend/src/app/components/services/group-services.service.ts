befe19fa2d1aa151a820378b9a0db5796be40452
Dominik <84574418+kerekdominik@users.noreply.github.com>
2024-11-02 23:33:16 +0100

    Group CRUD done

--- portfolio-frontend/src/app/components/services/group-services.service.ts
+++ portfolio-frontend/src/app/components/services/group-services.service.ts
@@ -3,6 +3,11 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/envrionment.dev';
 
+export interface Group {
+  id: number;
+  name: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,8 +16,21 @@ export class GroupService {
 
   constructor(private readonly http: HttpClient) { }
 
+  getAllGroups(): Observable<Group[]> {
+    return this.http.get<Group[]>(`${this.baseUrl}/groups`);
+  }
+
   createGroup(name: string): Observable<string> {
     const group = { name };
     return this.http.post<string>(`${this.baseUrl}/groups`, group, { responseType: 'text' as 'json' });
+  }
+
+  updateGroup(id: number, name: string): Observable<any> {
+    const group = { name };
+    return this.http.put<any>(`${this.baseUrl}/groups/${id}`, group);
+  }
+
+  deleteGroup(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.baseUrl}/groups/${id}`);
   }
 }
