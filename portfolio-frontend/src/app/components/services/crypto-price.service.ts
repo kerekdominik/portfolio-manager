9e6e54c2edb2f68cf07e64242d445d5252895d79
Dominik <84574418+kerekdominik@users.noreply.github.com>
2024-11-03 10:45:04 +0100

    Refactoring

--- portfolio-frontend/src/app/components/services/crypto-price.service.ts
+++ portfolio-frontend/src/app/components/services/crypto-price.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {environment} from '../../../environments/envrionment.dev';
 
@@ -11,12 +11,14 @@ export class CryptoPriceService {
 
   constructor(private readonly http: HttpClient) { }
 
-  getCurrentPrice(id: string): Observable<number> {
-    return this.http.get<number>(`${this.baseUrl}/crypto/current/price?id=${id}`);
+  getCurrentPrice(id: string): Observable<{ cryptoId: string, price: number }> {
+    const params = new HttpParams().set('id', id);
+    return this.http.get<{ cryptoId: string, price: number }>(`${this.baseUrl}/crypto/current/price`, { params });
   }
 
-  getOneYearAgoPrice(id: string, date: string): Observable<number> {
-    return this.http.get<number>(`${this.baseUrl}/crypto/historical/price?id=${id}&date=${date}`);
+  getOneYearAgoPrice(id: string, date: string): Observable<{ cryptoId: string, date: string, price: number }> {
+    const params = new HttpParams().set('id', id).set('date', date);
+    return this.http.get<{ cryptoId: string, date: string, price: number }>(`${this.baseUrl}/crypto/historical/price`, { params });
   }
 
   getAllCryptoNames(): Observable<string[]> {
